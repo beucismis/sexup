2ad5e337acdee1869555b5c0439cdfd462959bcc
beucismis <beucismis@tutamail.com>
2021-04-13 12:18:03 +0300

    Update __init__.py

--- backupill/__init__.py
+++ backupill/__init__.py
@@ -16,9 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import os
-import click
-from .backuper import Pill
+from backupill.util import *
 
 
 __version__ = "0.1.0"
@@ -27,26 +25,3 @@ __author__ = "Adil Gurbuz"
 __contact__ = "beucismis@tutamail.com"
 __source__ = "https://github.com/beucismis/backupill"
 __description__ = "Generates barcoded PDF to backup text files on paper."
-
-
-@click.version_option(version=__version__)
-@click.option("-f", "--file", help="ASC file path.")
-@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
-def main(file):
-    """Generates barcoded PDF to backup text files on paper.
-    https://github.com/beucismis/backupill
-    """
-
-    if file:
-        if not os.path.isfile(file):
-            click.echo("File '{}' not found!".format(file))
-            exit()
-
-        Pill(asc_file=file).generate_backup()
-        click.echo("Done!")
-    else:
-        click.echo("Usage: packupill -f FILENAME.asc")
-
-
-if __name__ == "__main__":
-    main()
