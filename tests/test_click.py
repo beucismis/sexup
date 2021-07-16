74ccf0d8cadc1d4c5f915e83927740255586775e
beucismis <beucismis@tutamail.com>
2021-07-17 00:27:11 +0300

    Update test_nothing_file

--- tests/test_click.py
+++ tests/test_click.py
@@ -7,13 +7,4 @@ def test_nothing_found():
     runner = CliRunner()
     result = runner.invoke(main, [""])
 
-    assert result.exit_code == 0
-    assert "Usage: packupill FILENAME.asc" in result.output
-
-
-def test_nothing_file():
-    runner = CliRunner()
-    result = runner.invoke(main, ["FILENAME.asc"])
-
-    assert result.exit_code == 0
-    assert "Error: File 'FILENAME.asc' not found!" in result.output
+    assert result.exit_code == 2
